cb833120ce1e33f57482f5af4b9a686caecb002a
Xzeni <n.delogu@studenti.unisa.it>
2024-03-15 13:38:40 +0100

    Refined main_bart.py

--- main_bart.py
+++ main_bart.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import DataLoader
 from transformers import BartTokenizer, BartForConditionalGeneration
 from model_bart import CustomDatasetBart
-from processing import analyze_topics, analyze_named_entities, most_common_words
+from processing import analyze_topics, analyze_named_entities
 
 SUMMARY_LEN = 20
 
@@ -38,7 +38,7 @@ def test_best_model():
     }
 
     test_dataset = CustomDatasetBart(abstracts=df_test["abstracts"], titles=df_test["titles"], tokenizer=tokenizer,
-                                 max_length=512)
+                                     max_length=512)
 
     test_loader = DataLoader(test_dataset, batch_size=1)
 
@@ -46,6 +46,9 @@ def test_best_model():
     bart_shared.eval()
 
     abstracts = df_test["abstracts"].tolist()
+
+    intersection_mean_original = []
+    intersection_mean_decoded = []
 
     with torch.no_grad():
         for index, batch in enumerate(test_loader):
@@ -61,38 +64,44 @@ def test_best_model():
             print(f"Original Title: {original_title}")
             print(f"Original Topics: {analyze_topics(original_title)}")
             print(f"Original Named Entities: {analyze_named_entities(original_title)}")
-            print(f"Original Most Common words: {most_common_words(original_title)}")
+
             print(f"Decoded Title: {decoded_title}")
             print(f"Decoded Topics: {analyze_topics(decoded_title)}")
             print(f"Decoded Named Entities: {analyze_named_entities(decoded_title)}")
-            print(f"Decoded Most Common Words: {most_common_words(decoded_title)}")
+
             print(f"Abstract Topics: {analyze_topics(abstracts[index])}")
             print(f"Abstract Named Entities: {analyze_named_entities(abstracts[index])}")
-            print(f"Abstract Most Common words: {most_common_words(abstracts[index])}")
 
             original_title_keywords = analyze_topics(original_title) + analyze_named_entities(original_title)
 
             decoded_title_keywords = analyze_topics(decoded_title) + analyze_named_entities(decoded_title)
 
-            abstract_keywords = analyze_topics(df_test["abstracts"][index]) + analyze_named_entities(df_test["abstracts"][index])
+            abstract_keywords = analyze_topics(df_test["abstracts"][index]) + analyze_named_entities(
+                df_test["abstracts"][index])
 
             intersection_original = calculate_intersection_percentage(abstract_keywords, original_title_keywords)
+            intersection_mean_original.append(intersection_original)
+
             intersection_decoded = calculate_intersection_percentage(abstract_keywords, decoded_title_keywords)
+            intersection_mean_decoded.append(intersection_decoded)
 
-            print(f"Intersection with Original Title: {intersection_original}%")
-            print(f"Intersection with Decoded Title: {intersection_decoded}%")
-
-            if intersection_decoded > intersection_original:
-                print("The decoded title better fits the abstract.")
-            elif intersection_decoded < intersection_original:
-                print("The original title better fits the abstract.")
-            else:
-                print("Both titles have the same fit to the abstract.")
+            print(f"Percentuale keywords titolo originale: {intersection_original}%")
+            print(f"Percentuale keywords titolo generato: {intersection_decoded}%")
 
             print("-------------------------------------")
+
+    sum_original = 0
+    sum_decoded = 0
+
+    for x in intersection_mean_original:
+        sum_original += x
+
+    for x in intersection_mean_decoded:
+        sum_decoded += x
+
+    print(f"Percentuale media topic titoli originali: {sum_original / len(intersection_mean_original)}%")
+    print(f"Percentuale media topic titoli generati: {sum_decoded / len(intersection_mean_decoded)}%")
 
 
 if __name__ == "__main__":
     test_best_model()
-
-
